8902214bfb37324938fcac4942b16a0116f90244
Jlesse <jlesseagile@gmail.com>
2016-01-17 15:31:06 -0600

    init commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import AppMap from "can-ssr/app-map";
+import route from "can/route/";
+import 'can/map/define/';
+import 'can/route/pushstate/';
+
+const AppViewModel = AppMap.extend({
+  // define: {
+  //   message: {
+  //     value: 'Hello World!',
+  //     serialize: false
+  //   },
+    title: {
+      value: 'place-my-order',
+      serialize: false
+    }
+  //}
+});
+
+can.route(":page", {page: "home"});
+route(':page/:slug', { slug: null });
+route(':page/:slug/:action', { slug: null, action: null });
+
+export default AppViewModel;
